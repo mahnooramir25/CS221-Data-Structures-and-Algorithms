2e9208e3a3cd37e906eab32e8876cb6a420a1ed1
mahnooramir25 <100989412+mahnooramir25@users.noreply.github.com>
2022-11-01 22:14:26 +0500

    Add files via upload

---
+++ Lab/Lab 1/Lab 1 Task 3.cpp
@@ -0,0 +1,19 @@
+//Using the concept of the dynamic array, prompt the user to enter the size of the array, prompt
+//the user to enter the elements of the array, and then show the array elements in reverse order.
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the size of array\n";
+    cin >> n;
+    int* arr = new int(n);
+    cout << "Enter elements in array\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "Elements in reverse order are:\n";
+    for (int i = n-1; i >= 0; i--) {
+        cout << arr[i]<<endl;
+    }
+    delete[] arr;
+}
